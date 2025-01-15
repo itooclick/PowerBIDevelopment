9f83ea0b7290f33fd308f03221d6f0ff275f55fc
itooclick <82318685+itooclick@users.noreply.github.com>
2025-01-15 14:07:52 +0530

    Add files via upload

--- SQL/Projects/Reports/Gross Sales Report Monthly for Amazon.sql
+++ SQL/Projects/Reports/Gross Sales Report Monthly for Amazon.sql
@@ -4,4 +4,4 @@ ON gp.product_code=fs.product_code
 AND gp.fiscal_year=gdb041.get_fiscal_year(date)
 where fs.customer_name like '%Amazon%'
 -- and fs.date between '2021-01-01 00:00:00' and '2021-12-31 00:00:00'
-Group by monthname(date) order by fs.date;
+Group by date order by fs.date;
