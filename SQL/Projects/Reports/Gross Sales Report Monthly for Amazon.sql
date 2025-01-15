0ccbaf8ab4b1b8c3feaf2e70b9e36113c91df1c9
itooclick <82318685+itooclick@users.noreply.github.com>
2025-01-15 13:59:32 +0530

    Add files via upload

---
+++ SQL/Projects/Reports/Gross Sales Report Monthly for Amazon.sql
@@ -0,0 +1,7 @@
+SELECT  date as Date ,ROUND(SUM((gp.gross_price*fs.sold_quantity))) As Gross_Price FROM 
+fact_sales_monthly fs  JOIN gdb056.gross_price gp
+ON gp.product_code=fs.product_code
+AND gp.fiscal_year=gdb041.get_fiscal_year(date)
+where fs.customer_name like '%Amazon%'
+-- and fs.date between '2021-01-01 00:00:00' and '2021-12-31 00:00:00'
+Group by monthname(date) order by fs.date;
